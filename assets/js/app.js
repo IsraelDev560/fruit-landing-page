6fd3759b39d1fee0778b74bc8793f09a55095c33
IsraelDev560 <devisrael560@gmail.com>
2024-06-20 01:54:22 -0300

    add: responsividade dentre outros

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,3 +58,12 @@ window.onload = function () {
         behavior: 'smooth' 
     });
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle')
+    const nav = document.querySelector('nav')
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active')
+    })
+})
